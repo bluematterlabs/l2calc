9e50c2f867f089631ac25c8b4ac6421a365a59d1
Turbo Panumarch <panumarch.a@gmail.com>
2022-08-03 17:59:40 +0700

    style the main panel

--- src/App.tsx
+++ src/App.tsx
@@ -105,59 +105,100 @@ function App() {
 
   return (
     <div className="h-screen">
-      <div className="container mx-auto text-white flex justify-center">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="w-60">
+          <img src={logo} alt="l2calc logo" />
+        </div>
+
+        <div className="bg-white py-16 px-24 rounded-md shadow-lg shadow-zinc-200/25">
           <div>L2 Transaction Data / L1 Tx Hash</div>
           <textarea
             rows={4}
             cols={53}
-            className={`search-box-a`}
             placeholder={`0x...`}
             onChange={onSearchChange}
+            className="w-full h-32 px-4 py-2 mb-2 text-base text-gray-700 placeholder-gray-600 border-2 rounded-lg focus:shadow-outline"
           />
-          <div className="tx-fee-box">
+
+          {/* background-image: radial-gradient( circle farthest-corner at 92.3% 71.5%,  rgba(83,138,214,1) 0%, rgba(134,231,214,1) 90% ); */}
+          <div
+            className="p-4 text-center text-xl rounded-sm mt-8 font-medium"
+            style={{
+              backgroundImage:
+                'radial-gradient( circle farthest-corner at 92.3% 71.5%,  #538ad67f 0%, #86e7d67f 90% )',
+            }}
+          >
             Tx Fee:{' '}
             {ethers.utils.formatUnits(
               l1SecurityFee.add(l2ExecutionFee),
               'ether',
             )}{' '}
-            eth
+            ETH
           </div>
-          <div className="gas-saving">{gasSavingWorld}</div>
-          <div className="l1-detail-area">
-            <div>
-              <span>L1 Gas Price: </span>
-              <span>{ethers.utils.formatUnits(l1GasPrice, 'gwei')} Gwei</span>
-            </div>
-            <div>
-              <span>L1 Gas Used: </span>
-              <span>{l1GasUsageField.toString()}</span>
-            </div>
-            <div>
-              <span>L1 Security Fee: </span>
-              <span>
-                {ethers.utils.formatUnits(l1SecurityFee, 'ether')} eth
-              </span>
-            </div>
+
+          <div className="text-center h-10 mt-2 text-transparent bg-clip-text bg-gradient-to-r from-[#5cd2bd] to-[#2c63ae] font-semibold ">
+            {gasSavingWorld}
           </div>
-          <div className="l2-detail-area">
-            <div>
-              <span>L2 Gas Price: </span>
-              <span>{ethers.utils.formatUnits(l2GasPrice, 'gwei')} Gwei</span>
-            </div>
-            <div>
-              <span>L2 Gas Used: </span>
-              <span>{l2GasUsage.toString()}</span>
-            </div>
-            <div>
-              <span>L2 Execution Fee: </span>
-              <span>
-                {ethers.utils.formatUnits(l2ExecutionFee, 'ether')} eth
-              </span>
-            </div>
-          </div>
-        </header>
+
+          <GasPriceDetail
+            l1GasPrice={l1GasPrice}
+            l1GasUsageField={l1GasUsageField}
+            l1SecurityFee={l1SecurityFee}
+            l2GasPrice={l2GasPrice}
+            l2GasUsage={l2GasUsage}
+            l2ExecutionFee={l2ExecutionFee}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+type Props = {
+  l1GasPrice: BigNumber
+  l1GasUsageField: BigNumber
+  l1SecurityFee: BigNumber
+  l2GasPrice: BigNumber
+  l2GasUsage: BigNumber
+  l2ExecutionFee: BigNumber
+}
+const GasPriceDetail: React.FC<Props> = ({
+  l1GasPrice,
+  l1GasUsageField,
+  l1SecurityFee,
+  l2GasPrice,
+  l2GasUsage,
+  l2ExecutionFee,
+}) => {
+  return (
+    <div>
+      <div className="l1-detail-area">
+        <div>
+          <span>L1 Gas Price: </span>
+          <span>{ethers.utils.formatUnits(l1GasPrice, 'gwei')} Gwei</span>
+        </div>
+        <div>
+          <span>L1 Gas Used: </span>
+          <span>{l1GasUsageField.toString()}</span>
+        </div>
+        <div>
+          <span>L1 Security Fee: </span>
+          <span>{ethers.utils.formatUnits(l1SecurityFee, 'ether')} eth</span>
+        </div>
+      </div>
+      <div className="l2-detail-area">
+        <div>
+          <span>L2 Gas Price: </span>
+          <span>{ethers.utils.formatUnits(l2GasPrice, 'gwei')} Gwei</span>
+        </div>
+        <div>
+          <span>L2 Gas Used: </span>
+          <span>{l2GasUsage.toString()}</span>
+        </div>
+        <div>
+          <span>L2 Execution Fee: </span>
+          <span>{ethers.utils.formatUnits(l2ExecutionFee, 'ether')} eth</span>
+        </div>
       </div>
     </div>
   )
